2fd16258ae2bb6adbe220976678efde29d0d6f48
1d4niGIT <ronaldds15@gmail.com>
2026-06-11 21:52:30 -0500

    Código Cámara

--- Assets/Scripts/ControlCámara.cs
+++ Assets/Scripts/ControlCámara.cs
@@ -1,29 +1,31 @@
 using UnityEngine;
-
 public class ControlCámara : MonoBehaviour
 {
     public GameObject Enfocado;
-    public float VelocidadCamara = 40f;
+    public float VelocidadCamara = 12f;
+    public float PPU = 200f;
+
+    private Vector3 _posicionReal;
+
     void Start()
     {
-        
+        _posicionReal = transform.position;
     }
 
     void Update()
     {
         Seguimiento();
     }
-    
+
     public void Seguimiento()
     {
-        /*// Seguir exactamente al jugador (sin suavizado)
-        Vector3 pos = Enfocado.transform.position;
-        pos.z = transform.position.z; // mantener Z de la cámara
-        transform.position = pos;*/
-
-        Vector3 dir = (Enfocado.transform.position - transform.position).normalized;
+        Vector3 dir = (Enfocado.transform.position - _posicionReal).normalized;
         dir.z = 0;
-        transform.position += dir * VelocidadCamara * Time.deltaTime;
+        _posicionReal += dir * VelocidadCamara * Time.deltaTime;
+
+        Vector3 posSnap = _posicionReal;
+        posSnap.x = Mathf.Round(posSnap.x * PPU) / PPU;
+        posSnap.y = Mathf.Round(posSnap.y * PPU) / PPU;
+        transform.position = posSnap;
     }
-    
 }
--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -1,25 +1,34 @@
 using UnityEngine;
-
 public class Jugador : MonoBehaviour
 {
-    public float VelocidadJugador = 2f;
+    public float VelocidadJugador = 5f;
     public float Vida = 100f;
+    public float PPU = 200f;  // puedes cambiarlo desde el Inspector
+
+    private Vector3 _posicionReal;
+
     void Start()
     {
-        
+        _posicionReal = transform.position;
     }
 
     void Update()
     {
         Movimiento();
     }
+
     public void Movimiento()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-        Vector3 dir = new Vector3(x, y, 0);
-        dir.Normalize();
+        Vector3 dir = new Vector3(x, y, 0).normalized;
+
         if (dir != Vector3.zero)
-            transform.position += dir * VelocidadJugador * Time.deltaTime;
+            _posicionReal += dir * VelocidadJugador * Time.deltaTime;
+
+        Vector3 posSnap = _posicionReal;
+        posSnap.x = Mathf.Round(posSnap.x * PPU) / PPU;
+        posSnap.y = Mathf.Round(posSnap.y * PPU) / PPU;
+        transform.position = posSnap;
     }
 }
